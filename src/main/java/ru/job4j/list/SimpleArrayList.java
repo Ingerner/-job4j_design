4fcbaec3e9958dc0fa1ae78dbd9eac0527a09ff1
Thinkpad <korotkiy.o@yandex.ru>
2022-01-20 23:06:56 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,91 @@
+package ru.job4j.list;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class SimpleArrayList<T> implements List<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modeCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            grow();
+        }
+        modeCount++;
+        container[size++] = value;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        modeCount++;
+        container[index] = newValue;
+        return newValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        modeCount++;
+        T removedElement = container[index];
+        for (int i = index; i < size; i++) {
+            if (i == size - 1) {
+                container[i] = null;
+            } else {
+                container[i] = container[i + 1];
+            }
+        }
+        size--;
+        return removedElement;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void grow() {
+        container = Arrays.copyOf(container, container.length * 2);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private final Iterator<T> data = Arrays.stream(Arrays.copyOf(container, size)).iterator();
+            
+            final int expectedModeCount = modeCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModeCount != modeCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return data.hasNext();
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return data.next();
+            }
+        };
+    }
+}
