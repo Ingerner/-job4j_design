51db1b670ba30909e6620744bc8e58b1ef6d6aec
Thinkpad <korotkiy.o@yandex.ru>
2022-01-27 22:28:49 +0300

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474 #256230]

--- src/main/java/ru/job4j/list/ForwardLinked.java
+++ src/main/java/ru/job4j/list/ForwardLinked.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
 
+
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
         if (head == null) {
@@ -20,7 +21,8 @@ public class ForwardLinked<T> implements Iterable<T> {
     }
 
     public void addFirst(T value) {
-        Node<T> node = new Node<T>(value, head);
+        Node<T> node = new Node<T>(value, null);
+        node.next = head;
         head = node;
     }
 
