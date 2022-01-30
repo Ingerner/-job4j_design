1f3595de12c0e350e781857873183435173d5245
Thinkpad <korotkiy.o@yandex.ru>
2022-01-30 20:33:17 +0300

    7. ListIterator [#350217 #257874]

--- src/main/java/ru/job4j/it/ListUtils.java
+++ src/main/java/ru/job4j/it/ListUtils.java
@@ -7,28 +7,18 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.nextIndex() == index) {
-                iterator.add(value);
-                break;
-            }
+        ListIterator<T> iterator = list.listIterator(index);
+        iterator.add(value);
             iterator.next();
         }
-    }
+
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator(list.size());
-        while (iterator.hasPrevious()) {
-            if (iterator.previousIndex() == index) {
-                iterator.add(value);
-                break;
-            }
-            iterator.previous();
+        ListIterator<T> iterator = list.listIterator(index + 1);
+        iterator.add(value);
+        iterator.previous();
         }
-
-    }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
         ListIterator<T> iterator = list.listIterator();
--- src/test/java/ru/job4j/it/ListUtilsTest.java
+++ src/test/java/ru/job4j/it/ListUtilsTest.java
@@ -17,7 +17,6 @@ public class ListUtilsTest {
     public void whenAddBefore() {
         List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
         ListUtils.addBefore(input, 1, 2);
-
         assertThat(input, is(Arrays.asList(1, 2, 3)));
     }
 
@@ -31,8 +30,6 @@ public class ListUtilsTest {
     public void whenAddAfterLast() {
         List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2));
         ListUtils.addAfter(input, 2, 3);
-
-
         assertThat(input, is(Arrays.asList(0, 1, 2, 3)));
     }
 
