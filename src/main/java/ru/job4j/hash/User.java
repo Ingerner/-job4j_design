ca97ed4516ee2f12e627e3114182ae1213ed8d8a
Thinkpad <korotkiy.o@yandex.ru>
2022-01-31 23:37:03 +0300

    1. Создать модель User [#999]

--- src/main/java/ru/job4j/hash/User.java
+++ src/main/java/ru/job4j/hash/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.hash;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -10,6 +13,30 @@ public class User {
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
         this.birthday = birthday;
     }
 }
