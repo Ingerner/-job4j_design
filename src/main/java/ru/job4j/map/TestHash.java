2f9eb9dd2a3feb3ca49cec1e89a53f0b584d6378
Thinkpad <korotkiy.o@yandex.ru>
2022-02-17 00:00:26 +0300

    переопределение hashcode

---
+++ src/main/java/ru/job4j/map/TestHash.java
@@ -0,0 +1,30 @@
+package ru.job4j.map;
+
+import java.util.Objects;
+
+public class TestHash {
+    private int age;
+    private String name;
+    private String surname;
+
+    public TestHash(int age, String name, String surname) {
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    public int hashCode() {
+        int rezult = Integer.hashCode(age);
+        rezult = 31 * rezult + name.hashCode();
+        rezult = 31 * rezult + surname.hashCode();
+        return rezult;
+    }
+
+
+    public static void main(String[] args) {
+        TestHash s = new TestHash(20, "Ivan", "Ivanov");
+        TestHash s1 = new TestHash(20, "Ivan", "Ivanov");
+        System.out.println(s.hashCode() +"     "  +s1.hashCode());
+    }
+}
