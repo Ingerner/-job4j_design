57e18355f7f17b6a56990752e5b4e2a50c1e249a
Thinkpad <korotkiy.o@yandex.ru>
2022-02-25 23:03:48 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -20,19 +20,15 @@ public class Analizy {
                         if (flag && "400".equals(array[0]) || "500".equals(array[0])) {
                             flag = false;
                             start = array[1];
+                            continue;
                         }
-                        if (!flag && ("400".equals(array[0]) || "500".equals(array[0]))) {
+                        if (!flag && !(("400".equals(array[0]) || "500".equals(array[0])))) {
                             flag = true;
                             end = array[1];
                             String interval = String.join(";", start, end);
                             writer.println(interval);
                         }
-
                     }
-                    // read.lines().forEach(values::add);
-//                    read.lines().map(s -> s.split(" ")).forEach(arr -> {
-//
-//                    } );
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -40,28 +36,6 @@ public class Analizy {
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        try (PrintWriter out1 = new PrintWriter(new FileOutputStream(target))) {
-//            String start = null;
-//            String end;
-//            for (String log : values) {
-//                String[] el = log.split(" ");
-//                String code = el[0];
-//                String time = el[1];
-//                if (start == null && ("400".equals(code) || "500".equals(code))) {
-//                    start = time;
-//                    continue;
-//                }
-//                if (start != null && ("200".equals(code) || "300".equals(code))) {
-//                    end = time;
-//                    String interval = String.join(";", start, end);
-//                    out.add(interval);
-//                    start = null;
-//                }
-//            }
-//            out1.println(out);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
     }
 
         public static void main(String[] args) {
