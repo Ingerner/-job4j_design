7749c6c7abb21e1bd31354146dc2a3b243901013
Thinkpad <korotkiy.o@yandex.ru>
2022-03-15 23:21:37 +0300

    4.2. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/DuplicatesFinder.java
@@ -5,7 +5,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class DuplicatesFinder {
+
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        DuplicatesVisitor visitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("C:\\projects"), visitor);
+        visitor.getDublicat().forEach(System.out::println);
     }
+
 }
--- src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -1,22 +1,42 @@
 package ru.job4j.io;
 
-import ru.job4j.map.Map;
-
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private final Map<FileProperty, Path> files = new HashMap<>();
+    private final Map<FileProperty, List<Path>> files = new HashMap<>();
+    private final List<FileProperty> dublicat = new ArrayList<>();
+
+    public List<List<Path>> getDublicat() {
+        return files.entrySet()
+                .stream()
+                .filter(el -> el.getValue().size() > 1)
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(attrs.size(), file.toFile().getName());
 
-        System.out.println(file.toAbsolutePath());
-        return super.visitFile(file, attrs);
+        if (!files.containsKey(fileProperty)) {
+            ArrayList paths = new ArrayList();
+            paths.add(file);
+            files.put(fileProperty, paths);
+        } else {
+            files.get(fileProperty).add(file);
+        }
+
+        return FileVisitResult.CONTINUE;
     }
 }
+
