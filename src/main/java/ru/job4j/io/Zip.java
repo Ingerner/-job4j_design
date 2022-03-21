0c0557cba88cc186fbc3a4067b9b5b934344bca9
Thinkpad <korotkiy.o@yandex.ru>
2022-03-21 22:54:36 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,18 +1,20 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public void packFiles(List<Path> sources, Path target) {
+    public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(
-                new FileOutputStream(target.toFile())))) {
-                for (Path path : sources) {
-                    zip.putNextEntry(new ZipEntry(path.toString()));
-                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                new FileOutputStream(target)))) {
+                for (File file : sources) {
+                    zip.putNextEntry(new ZipEntry(file.getPath()));
+                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
                         zip.write(out.readAllBytes());
                     }
                 }
@@ -21,9 +23,9 @@ public class Zip {
         }
     }
 
-    public void packSingleFile(File source, Path target) {
+    public void packSingleFile(File source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(
-                new FileOutputStream(target.toFile())))) {
+                new FileOutputStream(target)))) {
             zip.putNextEntry(new ZipEntry(source.getPath()));
             try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
                 zip.write(out.readAllBytes());
@@ -34,10 +36,23 @@ public class Zip {
     }
 
     public static void main(String[] args) {
+        ArgsName argsName = ArgsName.of(new String[] {args[0], args[1], args[2]});
+        String directory = argsName.get("d");
+        File f = new File(directory);
+        String delete = argsName.get("e");
+        String archive = argsName.get("o");
+        if (Files.exists(f.toPath())) {
+          //пишем исключение
+        }
+
         Zip zip = new Zip();
-        zip.packSingleFile(
-                new Path("./pom.xml"),
-                new Path("./pom.zip")
-        );
+//        zip.packSingleFile(
+//                new File("./pom.xml"),
+//                new File("./pom.zip")
+//        );
+        List<File> list = new ArrayList<>();
+        list.add(new File("./pom.xml"));
+        list.add(new File("./log.txt"));
+        zip.packFiles(list, new File("./files.zip"));
     }
 }
