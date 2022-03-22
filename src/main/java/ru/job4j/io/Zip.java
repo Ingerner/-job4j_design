5e580fe82b287813c82dd8a0f829fcd83f18fc42
Thinkpad <korotkiy.o@yandex.ru>
2022-03-22 23:13:22 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -14,12 +14,12 @@ public class Zip {
     public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(
                 new FileOutputStream(target)))) {
-                for (File file : sources) {
-                    zip.putNextEntry(new ZipEntry(file.getPath()));
-                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
-                        zip.write(out.readAllBytes());
-                    }
+            for (File file : sources) {
+                zip.putNextEntry(new ZipEntry(file.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+                    zip.write(out.readAllBytes());
                 }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -37,18 +37,26 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    private void validation() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
         ArgsName argsName = ArgsName.of(new String[]{args[0], args[1], args[2]});
         String directory = argsName.get("d");
-        File f = new File(directory);
+        File file = new File(directory);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.isAbsolute()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Is not a directory %s", file.isAbsolute()));
+        }
         String delete = argsName.get("e");
         String archive = argsName.get("o");
-        List<File> list = new ArrayList<>();
-        if (f.isDirectory()) {
-            for (File subfile : f.listFiles()) {
-               list.add(subfile);
-            }
-        }
+        List<File> list = Search.search(file.toPath(), p -> !p.getFileName().toString().endsWith(delete))
+                .stream()
+                .map(Path::toFile)
+                .toList();
         Zip zip = new Zip();
         zip.packFiles(list, new File(archive));
         }
