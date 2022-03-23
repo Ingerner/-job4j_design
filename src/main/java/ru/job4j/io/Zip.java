c2cb50dfac5e0a6eb8ed58d85314d62118118b74
Thinkpad <korotkiy.o@yandex.ru>
2022-03-23 21:49:23 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -25,7 +26,7 @@ public class Zip {
         }
     }
 
-    private void validation(File f) {
+    private void validation(File f, String expansion) {
 
         if (!f.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", f.isAbsolute()));
@@ -33,19 +34,22 @@ public class Zip {
         if (!f.isDirectory()) {
             throw new IllegalArgumentException(String.format("Is not a directory %s", f.isAbsolute()));
         }
+        if (!(Objects.equals('.', expansion.charAt(0)))) {
+            throw new IllegalArgumentException("расширение не соответствует");
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        ArgsName argsName = ArgsName.of(args);
         if (!(args.length == 3)) {
             throw new IllegalArgumentException("array is empty.");
         }
+        ArgsName argsName = ArgsName.of(args);
         String directory = argsName.get("d");
         String delete = argsName.get("e");
         String archive = argsName.get("o");
         File file = new File(directory);
         Zip zip = new Zip();
-        zip.validation(file);
+        zip.validation(file, delete);
         List<File> list = Search.search(file.toPath(), p -> !p.getFileName().toString().endsWith(delete))
                 .stream()
                 .map(Path::toFile)
