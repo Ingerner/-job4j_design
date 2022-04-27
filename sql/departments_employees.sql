4dbda0a81feae1c19ae5b026d9712773a2166f95
Oleg <korotkiy.o@yandex.ru>
2022-04-27 09:37:28 +0300

    0. Join [#6862 #293915]

--- sql/departments_employees.sql
+++ sql/departments_employees.sql
@@ -3,13 +3,13 @@ CREATE DATABASE departments_employees;
 CREATE TABLE departments(
     id serial primary key,
     name varchar(255)
-)
+);
 
 CREATE TABLE employees(
     id serial primary key,
     name varchar(255),
     departments_id int REFERENCES departments(id)
-)
+);
 
 INSERT INTO departments(name) VALUES('department-1'),
                                     ('department-2'),
@@ -40,7 +40,7 @@ CREATE TABLE teens(
 	id serial primary key,
 	name varchar(255),
 	gender char(1)
-)
+);
 
 
 INSERT INTO teens(name, gender) VALUES  ('Igor', 'm'), ('Sergey', 'm'), ('Egor', 'm'),
