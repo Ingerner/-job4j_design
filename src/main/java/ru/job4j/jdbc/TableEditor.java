e4506d8a81488018692edf89dbc41ee66f41ae49
Thinkpad <korotkiy.o@yandex.ru>
2022-04-27 23:55:24 +0300

    0.1. Statement [#379306]

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,100 @@
+package ru.job4j.jdbc;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws ClassNotFoundException, SQLException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        String url = properties.getProperty("url");
+        String user = properties.getProperty("login");
+        String password = properties.getProperty("password");
+        connection = DriverManager.getConnection(url, user, password);
+    }
+
+    public void createTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "create table if not exists %s(%s, %s);",
+                    tableName,
+                    "id serial primary key",
+                    "name text"
+            );
+            statement.execute(sql);
+            System.out.println(getTableScheme(connection, "tableName"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void dropTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "drop table %s;",
+                    tableName
+            );
+            statement.execute(sql);
+            //System.out.println(getTableScheme(connection, "tableName"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+    }
+
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "select * from %s limit 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Properties properties = new Properties();
+        properties.put("url", "jdbc:postgresql://localhost:5432/idea_db");
+        properties.put("login", "postgres");
+        properties.put("password", "*#*Job4j");
+        TableEditor editor = new TableEditor(properties);
+        editor.createTable("example_table");
+
+    }
+}
