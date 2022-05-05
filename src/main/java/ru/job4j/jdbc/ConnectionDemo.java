4fc9bc591f4bf1520aa8e555d1e7ee511163c0b5
Thinkpad <korotkiy.o@yandex.ru>
2022-05-05 23:30:33 +0300

    0. JDBC [#6863 #294201]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -2,16 +2,18 @@ package ru.job4j.jdbc;
 
 import ru.job4j.io.Config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDemo {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        Config config = new Config("C:\\projects\\-job4j_design\\src\\main\\resources\\app.properties");
+    public static void main(String[] args) throws ClassNotFoundException {
+        Config config = new Config(Config.class.getClassLoader().getResource("app.properties").getPath());
         config.load();
+        Class.forName(config.value("driver"));
         String url = config.value("url");
         String login = config.value("login");
         String password = config.value("password");
@@ -19,6 +21,8 @@ public class ConnectionDemo {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
             System.out.println(metaData.getURL());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
     }
 }
