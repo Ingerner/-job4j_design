0fad6c7755bccd98dfb469ea8db5feb76d522491
Oleg <korotkiy.o@yandex.ru>
2022-05-06 15:38:48 +0300

    Хранилище машин [#1733]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -52,7 +52,7 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Config("app.properties"));
+        System.out.println(new Config("./app.properties"));
     }
 
 }
