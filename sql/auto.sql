4f5d17667ddce3f6561af0dc4d29a39fc65b5c13
Oleg <korotkiy.o@yandex.ru>
2022-05-12 16:20:36 +0300

    Хранилище машин [#1733]

--- sql/auto.sql
+++ sql/auto.sql
@@ -1,57 +1,37 @@
-create table машина(
+create table kuzov(
     id serial primary key,
     name text
 );
 
-create table кузов(
+create table engine(
     id serial primary key,
-    кузов text,
-    id_машина int references машина(id)
+    name text  
 );
 
-create table двигатель(
+create table transmission(
     id serial primary key,
-    двигатель text,
-    id_машина int references машина(id)
+    name text
 );
 
-create table коробка_передач(
+create table car(
     id serial primary key,
-    коробка_передач text,
-    id_машина int references машина(id)
-);
+    name text,
+	id_kuzon int references kuzov(id),
+	id_egine int references engine(id),
+	id_transmission int references transmission(id)
+)
 
-insert into машина(name) values('бмв'), ('ауди'), ('форд'), ('тойота'), ('шевроле');
+insert into kuzov(name) values('седан'), ('купе'), ('универсал'), ('хетчбэк'), ('минивен');
 
-insert into кузов(кузов, id_машина) values('седан', 1), ('купе', 3), ('универсал', 5),
-										('хетчбэк',2), ('минивен', 3);
-insert into кузов(кузов) values('пикап');
+insert into engine(name) values('бензиновый'), ('газовый'), ('дизельный'), ('электрический');
 
-insert into двигатель(двигатель, id_машина) values('бензиновый', 1), ('газовый', 3),
-													('дизельный', 2), ('электрический',5);
-insert into двигатель(двигатель) values('водородный');
+insert into transmission(name) values('механическая'), ('роботизированная'), ('гидромеханическая'), ('вариатор');
 
-insert into коробка_передач(коробка_передач, id_машина) values('механическая', 5), ('роботизированная', 1),
-																('гидромеханическая', 4), ('вариатор',3);
-																
-select m.name, k.кузов, d.двигатель, kp.коробка_передач  from машина m left join кузов k
-on  m.id=k.id_машина 
-left join двигатель d
-on m.id=d.id_машина
-left join коробка_передач kp
-on m.id=kp.id_машина
-where коробка_передач;
+insert into car(name, id_kuzon, id_egine, id_transmission) values('бмв', 2, 1, 3),
+																	('ауди', 1, 3, 4),
+																	('форд', 3, 1, 1),
+																	('тойота', 3, 4, null),
+																	('шевроле', null, null, null);
 
-select k.кузов from кузов k left join машина m
-on k.id_машина=m.id
-where m.id is null;
 
-select d.двигатель from двигатель d left join машина m
-on d.id_машина=m.id
-where m.id is null;
 
-select kp.коробка_передач from коробка_передач kp left join машина m
-on kp.id_машина=m.id
-where m.id is null;
-
-																
