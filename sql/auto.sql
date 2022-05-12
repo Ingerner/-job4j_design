d809237197473ec6be9df9140e8e68fc706b7de4
Thinkpad <korotkiy.o@yandex.ru>
2022-05-12 21:54:24 +0300

    Хранилище машин [#1733 #299975]

--- sql/auto.sql
+++ sql/auto.sql
@@ -16,22 +16,41 @@ create table transmission(
 create table car(
     id serial primary key,
     name text,
-	id_kuzon int references kuzov(id),
+	id_kuzov int references kuzov(id),
 	id_egine int references engine(id),
 	id_transmission int references transmission(id)
-)
+);
 
 insert into kuzov(name) values('седан'), ('купе'), ('универсал'), ('хетчбэк'), ('минивен');
+insert into kuzov(name) values('пикап');
+
 
 insert into engine(name) values('бензиновый'), ('газовый'), ('дизельный'), ('электрический');
+insert into engine(name) values('водородный');
 
 insert into transmission(name) values('механическая'), ('роботизированная'), ('гидромеханическая'), ('вариатор');
 
-insert into car(name, id_kuzon, id_egine, id_transmission) values('бмв', 2, 1, 3),
+insert into car(name, id_kuzov, id_egine, id_transmission) values('бмв', 2, 1, 3),
 																	('ауди', 1, 3, 4),
 																	('форд', 3, 1, 1),
 																	('тойота', 3, 4, null),
 																	('шевроле', null, null, null);
+select c.name, k.name, e.name, t.name from car c left join kuzov k
+on c.id_kuzov=k.id
+left join engine e
+on c.id_egine=e.id
+left join transmission t
+on c.id_transmission=t.id;
 
+select k.name from kuzov k left join car c
+on k.id=c.id_kuzov
+where c.name is null;
 
+select e.name from engine e left join car c
+on e.id=c.id_kuzov
+where c.name is null;
+
+select t.name from transmission t left join car c
+on t.id=c.id_kuzov
+where c.name is null;
 
