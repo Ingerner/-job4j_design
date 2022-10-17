8ff3f442505e8063e80fc83c8252ca3fa196654b
Oleg <korotkiy.o@yandex.ru>
2022-10-17 16:37:44 +0300

    1. Представления [#504792]

---
+++ sql/authors.sql
@@ -0,0 +1,8 @@
+create table authors (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+insert into authors (name) values ('Лев Толстой');
