d62a44f221d2c70655e3d6e2eee320ac1d7af767
Oleg <korotkiy.o@yandex.ru>
2022-10-18 16:11:39 +0300

    1. Представления [#504792]

--- sql/request(view).sql
+++ sql/request(view).sql
@@ -1,3 +1,48 @@
+create table students (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into students (name) values ('Иван Иванов');
+insert into students (name) values ('Петр Петров');
+insert into students (name) values ('Сергей Сидоов');
+
+create table authors (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+insert into authors (name) values ('Лев Толстой');
+
+create table books (
+    id serial primary key,
+    name varchar(200),
+    author_id integer references authors(id)
+);
+
+insert into books (name, author_id) values ('Евгений Онегин', 1);
+insert into books (name, author_id) values ('Капитанская дочка', 1);
+insert into books (name, author_id) values ('Дубровский', 1);
+insert into books (name, author_id) values ('Мертвые души', 2);
+insert into books (name, author_id) values ('Вий', 2);
+insert into books (name, author_id) values ('Война и мир', 3);
+
+create table orders (
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books(id),
+    student_id integer references students(id)
+);
+
+insert into orders (book_id, student_id) values (1, 1);
+insert into orders (book_id, student_id) values (3, 1);
+insert into orders (book_id, student_id) values (5, 2);
+insert into orders (book_id, student_id) values (4, 1);
+insert into orders (book_id, student_id) values (2, 2);
+insert into orders (book_id, student_id) values (6, 3);
+
 create view show_how_many_books_each_student_has
 as select  s.name, count(b.name) from students as s
     join orders o on s.id = o.student_id
