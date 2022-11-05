46fd8eb656a946a4b594805a68e9fb87e0339bcb
Thinkpad <korotkiy.o@yandex.ru>
2022-11-06 01:39:21 +0300

    3. Хранимые процедуры и функции [#504804]

--- sql/products_procedure_function.sql
+++ sql/products_procedure_function.sql
@@ -33,4 +33,93 @@ as $$
 $$
 
 call update_data(10, 0, 1);
+call insert_data('product_1', 'producer_1', 3, 50);
+call insert_data('product_3', 'producer_3', 8, 115);
+
+alter procedure update_data(u_count integer, tax float, u_id integer) rename to update;
+
+drop procedure update(u_count integer, tax float, u_id integer);
+
+delete from products;
+ALTER SEQUENCE products_id_seq RESTART WITH 1;
+
+create or replace function f_insert_data(i_name varchar, prod varchar, i_count integer, i_price integer)
+returns void
+language 'plpgsql'
+as
+$$
+	BEGIN
+	insert into products(name, producer, count, price)
+	values(i_name, prod, i_count, i_price);
+	end;
+$$;
+
+drop function f_insert_data(i_name varchar, prod varchar, i_count integer, i_price integer);
+
+select f_insert_data('product_1', 'producer_1', 25, 50);
+
+drop function f_insert_data(u_count integer, tax float, u_id integer);
+
+create or replace function f_update_data(u_count integer, tax float, u_id integer)
+returns integer
+language 'plpgsql'
+as
+$$
+    declare
+        result integer;
+    begin
+        if u_count > 0 THEN
+            update products set count = count - u_count where id = u_id;
+            select into result count from products where id = u_id;
+        end if;
+        if tax > 0 THEN
+            update products set price = price + price * tax;
+            select into result sum(price) from products;
+        end if;
+        return result;
+    end;
+$$;
+	
+select f_update_data(10, 0, 1);
+
+select f_insert_data('product_2', 'producer_2', 15, 32);
+select f_insert_data('product_3', 'producer_3', 8, 115);
+
+select f_update_data(0, 0.2, 0);
+
+--//-------------------------------------------//--
+create or replace procedure delete_data(u_count integer, u_id integer)
+language  'plpgsql'
+as $$
+	BEGIN
+	IF u_count = 0 THEN
+	delete from products where id = u_id;
+	end IF;
+	END;
+$$
+
+drop procedure delete_data(u_count integer, u_id integer);
+
+select * from products;
+
+call delete_data(0, 1);
+
+--//------------------------------------//--
+create or replace function f_delete_data(u_count integer,u_id integer)
+returns void
+language 'plpgsql'
+as
+$$
+    begin
+        if u_count = 0 THEN
+           delete from products where id = u_id;
+        end if;
+    end;
+$$;
+
+select f_delete_data(0, 2);
+
+select * from products;
+
+
 	
