6061c1c52252458ac147e9a18ea1b4bddf977ec4
Oleg <korotkiy.o@yandex.ru>
2022-11-08 16:46:42 +0300

    4.3. Транзакции. Уровни изолированности транзакций. [#504812]

---
+++ sql/piople_transaction.sql
@@ -0,0 +1,13 @@
+create table human(
+	last_name varchar(50),
+	first_name varchar(50),
+	age integer
+);
+
+delete from human;
+
+insert into human(last_name, first_name, age) values('Sergey', 'Petrov', 30);
+insert into human(last_name, first_name, age) values('Andrey', 'Ivanov', 25);
+insert into human(last_name, first_name, age) values('Ivan', 'Sidorov', 35);
+
+select * from human;
