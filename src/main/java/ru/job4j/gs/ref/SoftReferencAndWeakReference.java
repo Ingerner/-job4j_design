413a5f1e0b35bd55d64698e8841f1af7d71cea82
Oleg <korotkiy.o@yandex.ru>
2022-12-07 16:47:49 +0300

    Пример безопасно работать с SoftReference и WeakReference

---
+++ src/main/java/ru/job4j/gs/ref/SoftReferencAndWeakReference.java
@@ -0,0 +1,40 @@
+package ru.job4j.gs.ref;
+
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SoftReferencAndWeakReference {
+
+    class Item {
+        private String name;
+
+        public Item(String name) {
+            this.name = name;
+        }
+    }
+
+    public boolean softReference() {
+        Scanner scanner = null;
+        SoftReference<String> name = new SoftReference<>(scanner.nextLine());
+        String nameItem = name.get();
+        if (name != null) {
+            Item item = new Item(nameItem);
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+
+    public void   weakReference(WeakReference<List<String>> list) {
+       List<String> link  = list.get();
+       if (link != null) {
+           for (String i : link) {
+               Item item = new Item(i);
+           }
+       }
+    }
+}
