6d4e0669fe8a05b227d5ae565fdefaf6568274a8
Ingener <korotkiy.o@yandex.ru>
2023-09-22 15:45:05 +0300

    2. Подключение библиотеки AssertJ [#504881]

---
+++ src/main/java/ru/job4j/ocp/GoodsDelivery.java
@@ -0,0 +1,15 @@
+package ru.job4j.ocp;
+
+
+public class GoodsDelivery {
+    boolean sum;
+    public void delivery(String s) {
+            /**
+            расчет суммы доставки
+             */
+        }
+   }
+
+
+
+
---
+++ src/main/java/ru/job4j/ocp/Transport.java
@@ -0,0 +1,17 @@
+package ru.job4j.ocp;
+
+public class Transport {
+    String city;
+
+    public Transport(String name) {
+        this.city = name;
+    }
+
+    public String getName() {
+        return city;
+    }
+
+    public void setName(String name) {
+        this.city = city;
+    }
+}
